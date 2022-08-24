d3f364d9ae382786b21ff0c01225585d03e8f404
osva <osantiago.hr@gmail.com>
2022-08-24 11:54:15 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1, 
+    title: "mi seg tarea",
+    description: "mi seg tarea",
+  },
+  {
+    id: 2,
+    title: "mi ter tarea",
+    description: "mi ter tarea",
+  },
+];
